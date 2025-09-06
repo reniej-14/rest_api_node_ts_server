0f852583db4137bebb5b3867b0eae99860a8efa8
reniej-14 <jeinerclash@gmail.com>
2025-09-05 22:25:55 -0600

    Creando un middleware

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { body } from 'express-validator'
 import { createProduct } from "./handlers/product"
+import { handleInputErrors } from "./middleware"
 
 const router = Router()
 
@@ -17,7 +18,7 @@ router.post('/',
         .isNumeric().withMessage('Valor no válido')
         .notEmpty().withMessage('El precio del producto no puede ir vacio')
         .custom(value => value > 0).withMessage('Precio no válido'),
-
+        handleInputErrors,
     createProduct
 )
 
