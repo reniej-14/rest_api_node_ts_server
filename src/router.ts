1293511d62308c3ec36d5ea70a595e7d952c28ad
reniej-14 <jeinerclash@gmail.com>
2025-09-05 22:05:31 -0600

    Agregando validacion en el router

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { body } from 'express-validator'
 import { createProduct } from "./handlers/product"
 
 const router = Router()
@@ -8,7 +9,17 @@ router.get('/', (req, res) => {
     res.json('Desde get')
 })
 
-router.post('/', createProduct)
+router.post('/', 
+    // Validación
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
+    body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El precio del producto no puede ir vacio')
+        .custom(value => value > 0).withMessage('Precio no válido'),
+
+    createProduct
+)
 
 router.put('/', (req, res) => {
     res.json('Desde put')
