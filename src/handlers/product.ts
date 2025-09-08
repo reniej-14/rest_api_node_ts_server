6570f31de71bccf38c37b61b383ecc9bae5e19de
reniej-14 <jeinerclash@gmail.com>
2025-09-07 23:01:14 -0600

    Creando un nuevo endpoint

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from 'express'
 import Product from '../models/Product.model'
 
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll({
+            attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
+        })
+        res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export const createProduct = async (req : Request, res : Response) => {
 
     try {
